ce47e2a64d4d3791c1fac31102aee5bdf528015a
Nikol Paraskova <nicoleparaskova@gmail.com>
2018-01-08 17:03:13 +0200

    [add-ng-services-and-controllers] - Refactoring the code in a way of adding controllers and services

---
+++ public/js/controllers/main.js
@@ -0,0 +1,37 @@
+angular.module('todoController', [])
+
+    .controller('mainController', function($scope, $http, Todos) {
+        $scope.formData = {};
+    
+        // when landing on the page, get all todos and show them
+        //using the service to get all the todos
+
+        Todos.get()
+            .success(function(data){
+                $scope.todos = data;
+            });
+    
+        // when submitting the add form, send the text to the node API
+       
+        $scope.createTodo = function() {
+
+            if(!$.isEmptyObject($scope.formData)){
+
+                Todos.create($scope.formData)
+
+                .success(function(data){
+                    $scope.formData = {};
+                    $scope.todos = data;
+                });
+            }
+        };
+    
+        // delete a todo after checking it
+        $scope.deleteTodo = function(id) {
+            Todos.delete(id)
+                .success(function(data) {
+                    $scope.todos = data;
+            });
+        };
+    
+    });
